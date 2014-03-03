34cdc0061b268adb41c3517f62d8da40fbbe14d0
Eric Watson <wasnotrice@gmail.com>
2014-03-03 11:27:06 -0600

    Add pothole layer

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 (function($) {
-  var map = L.map('map').setView([46.7868843, -92.1215788], 13);
-
+  // Base layers
   // See http://leaflet-extras.github.io/leaflet-providers/preview/
   var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
   });
- var OpenStreetMap_HOT = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+  var OpenStreetMap_HOT = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
   })
+  var base = OpenStreetMap_Mapnik;
 
-  OpenStreetMap_Mapnik.addTo(map);
+  var potholeOptions = {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+  };
+  var potholes = L.layerGroup([
+    L.circle([46.7868843, -92.1215788], 1, potholeOptions)
+  ]);
+
+  var map = L.map('map', {
+    center: new L.LatLng(46.7868843, -92.1215788),
+    zoom: 13,
+    layers: [base, potholes]
+  });
 })(jQuery);
 
