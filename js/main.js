3fe10bd4269de6f5a6dc5eb045ca40e05720d350
Eric Watson <wasnotrice@gmail.com>
2014-03-03 13:08:50 -0600

    Reorganize main.js for readability

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
 (function($) {
-  // Base layers
-  // See http://leaflet-extras.github.io/leaflet-providers/preview/
-  var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-  });
-  var OpenStreetMap_HOT = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-   attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
-  })
-  var base = OpenStreetMap_Mapnik;
-
-  var potholeOptions = {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5
-  };
-  var potholes = L.layerGroup([
-    L.circle([46.7868843, -92.1215788], 1, potholeOptions)
-  ]);
-
+  var base = baseLayers();
+  var potholes = potholeLayer();
   var map = L.map('map', {
     center: new L.LatLng(46.7868843, -92.1215788),
     zoom: 13,
-    layers: [base, potholes]
+    layers: [base.OpenStreetMap_Mapnik, potholes]
   });
+
+  // See http://leaflet-extras.github.io/leaflet-providers/preview/
+  function baseLayers() {
+    return {
+      OpenStreetMap_Mapnik: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+      }),
+      OpenStreetMap_HOT: L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+       attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+      })
+    };
+  }
+
+  function potholeLayer() {
+    var potholeCoordinates = [
+      [46.786243, -92.101092],
+      [46.787984, -92.102712]
+    ];
+    var potholeOptions = {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5
+    };
+    var potholeCircles = [];
+    for (var i = 0; i < potholeCoordinates.length; i++) {
+      potholeCircles.push(L.circle(potholeCoordinates[i], 1, potholeOptions));
+    }
+
+    return L.layerGroup(potholeCircles);
+  }
 })(jQuery);
 
