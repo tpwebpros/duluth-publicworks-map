a3c1f224db6f731610432d15f659f32fa459cac1
Eric Watson <wasnotrice@gmail.com>
2014-03-03 11:02:51 -0600

    Add Leaflet, initial map

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+(function($) {
+  var map = L.map('map').setView([46.7868843, -92.1215788], 13);
+
+  // See http://leaflet-extras.github.io/leaflet-providers/preview/
+  var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+  });
+ var OpenStreetMap_HOT = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+   attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+  })
+
+  OpenStreetMap_Mapnik.addTo(map);
+})(jQuery);
 
